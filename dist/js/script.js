dd0831a97c4733206ecd5a440b39cff430c0c7cf
Daniil <sidorchuk.daniil@gmail.com>
2022-10-22 21:05:42 +0300

    Commit 7. Started to create responsive markup. Made section 'promo'

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
+    // Hamburger
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.header__menu_mobile');
 
-// Slider
+          hamburger.addEventListener('click', () => {
+            menu.classList.toggle('active');
+          });
+
+    // Slider
     const slider = tns({
         container: '.feedback__slider',
         items: 1,
         startIndex: 1,
         center: true,
-        rewind: true,
+        rewind: true, // Переместиться в начало или в конец, когда долистали
         swipeAngle: false,
         autoplay: false,
         nav: false,
